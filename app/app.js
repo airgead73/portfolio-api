df80fa13596339c2def2bdbd774fa8c8f4e6ea28
Dan Moneypenny <catullus1973@gmail.com>
2022-11-24 00:22:55 -0800

    add basic app

---
+++ app/app.js
@@ -0,0 +1,49 @@
+/**
+ * external imports
+ */
+ const express = require('express');
+ const path = require('path');
+ const cookieParser = require('cookie-parser');
+/**
+ * internal imports
+ */
+/**
+ * app activation
+ */
+const app = express();
+/**
+ * middleware
+ */
+ app.use(express.static(path.join(__dirname, 'public')));
+ app.use(express.json({ limit: '50mb' }));
+ app.use(express.urlencoded({ limit: '50mb', extended: true }));
+ app.use(cookieParser()); 
+/**
+ * locals
+ */
+/**
+ * routes
+ */
+/**
+ * error handling
+ */
+ app.use(function(req, res, next) {
+  const error = new Error('path not found.');
+  next(error);
+});
+
+app.use(function(err, req, res, next) {
+  let status;
+  if(!err.status) {
+    status = 500;
+  } else {
+    status = err.status;
+  }
+
+  res.send(`ERROR: ${err.message}`);
+
+});
+/**
+ * export app
+ */
+module.exports = app;
